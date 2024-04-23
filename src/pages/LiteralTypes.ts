a176c1373f00e9ac35c6331b234775e30e09d812
Kim Hobin <isb0573@naver.com>
2024-04-23 19:00:21 +0900

    function, Literal, interface

---
+++ src/pages/LiteralTypes.ts
@@ -0,0 +1,64 @@
+// Literal Types
+
+const userName1 = "Bob";
+let userName2: string | number = "Tom";
+userName2 = 3;
+
+type Job = "police" | "developer" | "teacher";
+
+interface User2 {
+  name: string;
+  job: string;
+}
+
+const user: User2 = {
+  name: "Bob",
+  job: "deveoloper",
+};
+
+interface HighSchoolStudent {
+  name: number | string;
+  grade: 1 | 2 | 3;
+}
+
+// Union Types
+interface Car {
+  name: "Car";
+  color: string;
+  start(): void;
+}
+
+interface Mobile {
+  name: "Mobile";
+  color: string;
+  call(): void;
+}
+
+function getGift(gift: Car | Mobile) {
+  console.log(gift.color);
+  if (gift.name === "Car") {
+    gift.start();
+  } else {
+    gift.call();
+  }
+}
+
+// Intersection Types
+
+interface Car2 {
+  name: string;
+  start(): void;
+}
+
+interface Toy {
+  name: string;
+  color: string;
+  price: number;
+}
+
+const toyCar: Toy & Car2 = {
+  name: "타요",
+  start() {},
+  color: "blue",
+  price: 1000,
+};
---
+++ src/pages/interface.ts
@@ -0,0 +1,40 @@
+// interface
+interface Add {
+  (num1: number, num2: number): number;
+}
+
+const add: Add = function (x, y) {
+  return x + y;
+};
+
+add(10, 20);
+
+interface IsAdult {
+  (age: number): boolean;
+}
+
+const a: IsAdult = (age) => {
+  return age > 19;
+};
+
+interface Car {
+  color: string;
+  wheels: number;
+  start(): void;
+}
+
+class Bmw implements Car {
+  color;
+  wheels = 4;
+  constructor(c: string) {
+    this.color = c;
+  }
+  start() {
+    console.log("go...");
+  }
+}
+
+const b = new Bmw("green");
+
+console.log(b);
+b.start();
